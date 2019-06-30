7f94cb95d69a103d80b48587e8beb2f0050eb281
harshinibaskar <51949520+harshinibaskar@users.noreply.github.com>
2019-06-30 08:33:19 +0530

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,5 @@
+s=input()
+if(s=='a'or'b'or'c'or'd'or'e'or'f'or'g'or'h'or'i'or'j'or'k'or'l'or'm'or'n'or'o'or'p'or'q'or'r'or's'or't'or'u'or'v'or'w'or'x'or'y'or'z'):
+   print("Alphabet")
+else:
+   print("No")
