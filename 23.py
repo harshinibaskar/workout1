97be1c2d0f6f68b8b9cce0e1e2b3f1e93c016659
harshinibaskar <51949520+harshinibaskar@users.noreply.github.com>
2019-07-03 14:43:14 +0530

    Update 23.py

--- 23.py
+++ 23.py
@@ -2,9 +2,9 @@ a,b=map(int,input().split())
 for f in range(a+1,b)
     ch=f
     find=0
- while(f>0):
-    v=f%10
-    find=find+(v**3)
-    f=f//10
-    if(find==ch):
+  while(f>0):
+   v=f%10
+   find=find+(v**3)
+   f=f//10
+   if(find==ch):
       print(find,end" ")
